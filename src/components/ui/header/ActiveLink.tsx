c81b6c83c965279a65358e43fcc06288d8136344
Luis Fernando Cruces Vidal <luis.cruces@instructure.com>
2023-05-30 09:17:54 -0500

    fix: navbar style

--- src/components/ui/header/ActiveLink.tsx
+++ src/components/ui/header/ActiveLink.tsx
@@ -9,10 +9,13 @@ interface Props {
 
 export const ActiveLink: React.FC<Props> = ({ text, href }) => {
   const pathname = usePathname();
+  const stylesForHomePage = pathname === '/' ? 'text-white' : 'text-black';
   return (
     <Link
       href={href}
-      className={`${pathname === href ? 'text-primary font-bold' : ''} px-2 `}
+      className={`${
+        pathname === href ? 'text-primary font-bold' : stylesForHomePage
+      } px-2`}
     >
       {text}
     </Link>
